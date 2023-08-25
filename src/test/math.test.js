a75a04e7919f321fdda02f2d80e7b950f4dd4aa5
djfor5 <117873331+djfor5@users.noreply.github.com>
2023-08-26 09:28:07 +1000

    Add eslint

--- src/test/math.test.js
+++ src/test/math.test.js
@@ -1,5 +1,7 @@
 const { add, subtract, multiply } = require("../math.js");
 
+/* eslint-disable no-undef */
+
 test("Expect 2 + 3 = 5", () => {
   expect(add(2, 3)).toEqual(5);
 });
@@ -11,3 +13,5 @@ test("Expect 5 - 2 = 3", () => {
 test("Expect 2 * 3 = 6", () => {
   expect(multiply(2, 3)).toEqual(6);
 });
+
+/* eslint-enable no-undef */
